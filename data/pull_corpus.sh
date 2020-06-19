9864e5b5290d73e1ad53585b64fc5927426e7e5d
ncalderon <nathaniel.calderon@gmail.com>
2020-06-19 11:34:07 +0200

    sending changes

--- data/pull_corpus.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget --post-data="passwd-fichero=pt27xQr%2149&x=7&y=13" -c 'https://consigna.us.es/547743/descarga' -O PLN-MULCIA-Junio-2020-Dataset.zip
-unzip PLN-MULCIA-Junio-2020-Dataset.zip && mv -v PLN-MULCIA-Junio-2020-Dataset/* ./
-rm -r PLN-MULCIA-Junio-2020-Dataset/
-rm PLN-MULCIA-Junio-2020-Dataset.zip
--- example/run_custom.sh
+++ example/run_custom.sh
@@ -1,6 +1,8 @@
 cd punctuator2/example/
 wget --post-data="passwd-fichero=pt27xQr%2149&x=7&y=13" -c 'https://consigna.us.es/547743/descarga' -O PLN-MULCIA-Junio-2020-Dataset.zip
 unzip PLN-MULCIA-Junio-2020-Dataset.zip && mv -v PLN-MULCIA-Junio-2020-Dataset/* ./
+rm -r PLN-MULCIA-Junio-2020-Dataset/
+rm PLN-MULCIA-Junio-2020-Dataset.zip
 rm -rf ./out
 echo "Step 1/3"
 mkdir ./out
